43f54e6a45f677af4e5a046a28b638713a912a3f
nitesh091 <nitesh.srivastava@twinspark.co>
2016-08-02 13:27:32 +0530

    api doc

---
+++ api_data.js
@@ -0,0 +1,168 @@
+define({ "api": [
+  {
+    "type": "post",
+    "url": "/users/add",
+    "title": "Register New User",
+    "name": "add",
+    "group": "Users",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>User's email ID.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>User's Password.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "Name",
+            "description": "<p>User's Name.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolen",
+            "optional": false,
+            "field": "Status",
+            "description": "<p>User's Status.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "success",
+            "description": "<p>status of the request.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n \"success\": true,\n  \"data\": {\n          \"id\": 2\n  }      \n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "MandatoryParamMissing",
+            "description": "<p>Mandatory Param missing</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n \"success\":false,\n \"message\":\"Mandatory param missing.\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/Controller/UsersController.php",
+    "groupTitle": "Users"
+  },
+  {
+    "type": "post",
+    "url": "/users/login",
+    "title": "Login User",
+    "version": "1.0.0",
+    "name": "login",
+    "group": "Users",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Users email ID.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Users Password.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "status",
+            "description": "<p>status of the request.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID of the User.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "token",
+            "description": "<p>Auth Token.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n \"success\": true,\n  \"data\": {\n          \"id\": 2,\n          \"token\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIsImV4cCI6MTQ3MDIyNjI5M30.InrHIhgBAae6gJ9nTXaN5CV4oZhxcOvAv0fyS8DJ7Kw\"\n  }      \n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n \"status\":false,\n \"message\":\"Invalid User!: You should not be peeking around here.\"\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "InvalidCredential",
+            "description": "<p>Provided Credentials didn't match with any account.</p>"
+          }
+        ]
+      }
+    },
+    "filename": "src/Controller/UsersController.php",
+    "groupTitle": "Users"
+  }
+] });
---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "People Hub",
+  "version": "1.0.0",
+  "description": "People Hub Apis",
+  "title": "People Hub Apis",
+  "url": "https://peoplehub.twinspark.co/peoplehub",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-08-02T07:51:34.708Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});

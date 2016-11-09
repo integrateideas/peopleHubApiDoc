b8b6f4d398c489f76bd13fa44788b4db0eae94f0
nitesh srivastava <nitesh.srivastava@twinspark.co>
2016-11-09 12:13:31 +0530

    updated doc

--- api_project.js
+++ api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-11-08T09:58:36.677Z",
+    "time": "2016-11-09T06:43:09.940Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
